3c7e950e61ebbc6a481bc6843f1431e96a5a776f
qiuchunji <xiaofishy@163.com>
2021-11-19 19:04:40 +0800

    update load test case

---
+++ TransactionByHashReport/run.sh
@@ -0,0 +1,3 @@
+rm -f log$1-$2-$3-$4
+rm -rf report$1-$2-$3-$4
+jmeter -n -t  load_test_eth_getTransactionByHash.jmx -l ./log$1-$2-$3-$4 -e -o ./report$1-$2-$3-$4  -Dtest.threads=$1 -Dtest.nextadd=$2 -Dtest.runtime=$3 -Dtest.holdtime=$4
